4e013274c06bf61f1cf3ef8ffdef43be609ced1d
melodicoasis9 <melodicoasis9@gmail.com>
2026-07-06 17:33:07 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-const CACHE = 'tango-v2';
-const FILES = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
+const CACHE = 'tango-v4';
+const FILES = [
+  '/apps/',
+  '/apps/index.html',
+  '/apps/manifest.json',
+  '/apps/icon-192.png',
+  '/apps/icon-512.png'
+];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
