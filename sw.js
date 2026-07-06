8aba9aff94b1b51e141775e81b7abac81a654118
melodicoasis9 <melodicoasis9@gmail.com>
2026-07-06 18:55:16 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,33 +1,11 @@
-const CACHE = 'tango-v6';
-const FILES = [
-  '/apps/',
-  '/apps/index.html',
-  '/apps/manifest.json',
-  '/apps/icon-192.png',
-  '/apps/icon-512.png'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
-  self.skipWaiting();
-});
-
+// v7 - no cache, always fetch fresh
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  // Always fetch fresh from network, fall back to cache
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
