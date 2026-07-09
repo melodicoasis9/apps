285ad09dba1d51ab4e05e0943579e99928cea97f
melodicoasis9 <melodicoasis9@gmail.com>
2026-07-09 18:07:50 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,7 @@
-// v7 - no cache, always fetch fresh
+// tango-v9 - no cache, always fetch fresh
 self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
   self.clients.claim();
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request));
-});
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request)); });
